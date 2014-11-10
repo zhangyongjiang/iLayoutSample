e31c56ca2f160633f1d5fca358b9dd573d579d62
Kevin Zhang <zhangyongjiang@gmail.com>
2014-11-10 08:19:51 -0800

    initProperties is no longer needed

--- iLayoutSample/RegisterView.m
+++ iLayoutSample/RegisterView.m
@@ -10,17 +10,22 @@
 
 @implementation RegisterView
 
--(void)initProperties {
-    self.btnSignup = [[UIButton alloc] init];
-    self.firstNameField = [[UITextField alloc] init];
-    self.lastNameField = [[UITextField alloc] init];
-    self.emailField = [[UITextField alloc] init];
-    self.passwordField = [[UITextField alloc] init];
-    self.locationField = [[UITextField alloc] init];
-    
-    NSArray* views = [NSArray arrayWithObjects:self.btnSignup, self.firstNameField, self.lastNameField, self.emailField, self.passwordField, self.locationField, nil];
-    [self enableCssLayouts:views :YES];
-    [self addSubviews:views];
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.btnSignup = [[UIButton alloc] init];
+        self.firstNameField = [[UITextField alloc] init];
+        self.lastNameField = [[UITextField alloc] init];
+        self.emailField = [[UITextField alloc] init];
+        self.passwordField = [[UITextField alloc] init];
+        self.locationField = [[UITextField alloc] init];
+        
+        NSArray* views = [NSArray arrayWithObjects:self.btnSignup, self.firstNameField, self.lastNameField, self.emailField, self.passwordField, self.locationField, nil];
+        [self enableCssLayouts:views :YES];
+        [self addSubviews:views];
+    }
+    return self;
 }
 
 @end
